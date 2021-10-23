cf0a15348fd4e36d6229e43c1a0f66bf7293eff7
Dario Pavlovic <dario.pavlovic@fer.hr>
2021-10-23 17:44:17 +0200

    Incijalna struktura backenda
    Dodana incijalna struktura backenda.

---
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/MojkvartApplication.java
@@ -0,0 +1,13 @@
+package progi.project.mojkvart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojkvartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojkvartApplication.class, args);
+	}
+
+}
---
+++ IzvorniKod/backend-spring-boot/src/test/java/progi/project/mojkvart/MojkvartApplicationTests.java
@@ -0,0 +1,13 @@
+package progi.project.mojkvart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojkvartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
