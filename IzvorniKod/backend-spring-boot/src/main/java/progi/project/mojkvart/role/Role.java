5eb7229f75a6772f2e4925917b806007d08151db
Danijel Barišić <danijel.bar17@gmail.com>
2021-11-04 12:13:52 +0100

    Dodan Role model

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/role/Role.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/role/Role.java
@@ -1,4 +1,64 @@
 package progi.project.mojkvart.role;
 
+import progi.project.mojkvart.role_request.RoleRequest;
+import progi.project.mojkvart.user.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    public Role(){
+
+    }
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    // lazy FetchType because we don't want to immediately fetch all users that have the role we're fetching
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+
+    @OneToMany(mappedBy = "role")
+    private List<RoleRequest> roleRequests;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
 }
