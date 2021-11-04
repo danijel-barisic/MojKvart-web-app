d4dda829fcd00901c48232d5b55a5590290b04f3
Danijel Barišić <danijel.bar17@gmail.com>
2021-11-04 12:14:30 +0100

    Dodan RoleRequest model

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/role_request/RoleRequest.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/role_request/RoleRequest.java
@@ -1,4 +1,59 @@
 package progi.project.mojkvart.role_request;
 
+import progi.project.mojkvart.role.Role;
+import progi.project.mojkvart.user.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="role_request")
 public class RoleRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_request_id")
+    private Long id;
+
+    @Column(name="role_request_status")
+    private String roleRequestStatus;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="role_id")
+    private Role role;
+
+    public RoleRequest() {
+
+    }
+
+    public RoleRequest(String roleRequestStatus) {
+        this.roleRequestStatus = roleRequestStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleRequestStatus() {
+        return roleRequestStatus;
+    }
+
+    public void setRoleRequestStatus(String roleRequestStatus) {
+        this.roleRequestStatus = roleRequestStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleRequest{" +
+                "id=" + id +
+                ", roleRequestStatus='" + roleRequestStatus + '\'' +
+                '}';
+    }
 }
