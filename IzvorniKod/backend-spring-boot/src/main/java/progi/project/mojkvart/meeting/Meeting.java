bb5720912948a5f3d8efac0417d358f0b5b914f6
Dario Pavlovic <dario.pavlovic@fer.hr>
2021-11-06 10:45:08 +0100

    Ažuriran model Meeting

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/meeting/Meeting.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/meeting/Meeting.java
@@ -27,4 +27,44 @@ public class Meeting {
     @OneToMany(mappedBy = "meeting")
     private List<User> users;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
+    public PostThread getPostThread() {
+        return postThread;
+    }
+
+    public void setPostThread(PostThread postThread) {
+        this.postThread = postThread;
+    }
+
+    public Council getCouncil() {
+        return council;
+    }
+
+    public void setCouncil(Council council) {
+        this.council = council;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
 }
