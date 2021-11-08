ff5b5c3ab73745d7ab492f233c83cdbdee66698a
Theanko <danko.curlin@hotmail.com>
2021-11-08 12:35:37 +0100

    Event template

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/event/Event.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/event/Event.java
@@ -1,4 +1,14 @@
 package progi.project.mojkvart.event;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "event")
 public class Event {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "test")
+    private Long id;
 }
