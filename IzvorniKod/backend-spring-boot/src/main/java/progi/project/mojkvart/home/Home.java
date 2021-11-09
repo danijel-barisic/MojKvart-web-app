c68888898d3af7b8464a5c732456788594a7dcae
HeliumHe-He <bartol.hrg.backup@gmail.com>
2021-11-09 14:34:24 +0100

    dodan Home

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/home/Home.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/home/Home.java
@@ -1,4 +1,59 @@
 package progi.project.mojkvart.home;
 
+
+import org.springframework.lang.NonNull;
+import progi.project.mojkvart.street.Street;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "home")
 public class Home {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "home_id")
+    private Long id;
+
+    @Column(name = "home_number")
+//    @NonNull
+    private Long homeNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "street_id", table = "street")
+    private Street street;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getHomeNumber() {
+        return this.homeNumber;
+    }
+
+    public void setHomeNumber(Long home_number) {
+        this.homeNumber = home_number;
+    }
+
+    public Street getStreet() {
+        return this.street;
+    }
+
+    public void setStreet(Street street) {
+        this.street = street;
+    }
+
 }
+
+/*CREATE TABLE home
+(
+  home_id INT NOT NULL,
+  home_number INT NOT NULL,
+  street_id INT NOT NULL,
+
+  PRIMARY KEY (home_id),
+  FOREIGN KEY (street_id) REFERENCES street(street_id)
+);*/
---
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/home/HomeRepository.java
@@ -0,0 +1,11 @@
+package progi.project.mojkvart.home;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HomeRepository extends JpaRepository<Home, Long> {
+    Optional<Home> findById(Long id);
+
+    boolean existsById(Long id);
+}
