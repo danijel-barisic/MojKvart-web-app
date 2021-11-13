843c3229fa13646e861dfaea743ac249550361bc
Theanko <danko.curlin@hotmail.com>
2021-11-13 13:43:44 +0100

    Popravljene greske prilikom mergea
    (event)

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/post/Post.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/post/Post.java
@@ -8,6 +8,7 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "post")
