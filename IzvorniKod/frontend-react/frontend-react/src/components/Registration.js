2a6c0826470894beca04538867c05b0d9e37b2b1
Andrija Banic <andrija.banic@fer.hr>
2021-12-06 16:01:48 +0100

    Prva verzija unosa adrese prilikom registracije

--- IzvorniKod/frontend-react/frontend-react/src/components/Registration.js
+++ IzvorniKod/frontend-react/frontend-react/src/components/Registration.js
@@ -3,18 +3,71 @@ import Card from "./Card";
 import './Login.css';
 import './Card.css';
 import { useHistory } from "react-router";
+import Select from 'react-select';
+
+
+
+
+//https://react-select.com/styles#styles
+ const customStyles = {
+   option: (provided, state) => ({
+     ...provided,
+     padding: 20,
+     
+   }),
+   menuList: styles => ({
+      
+      
+        ...styles,
+        maxHeight: 138
+      }),
+   control: styles => ({ ...styles, backgroundColor: 'white',borderRadius:'10px' }),
+   singleValue: (provided, state) => {
+     const opacity = state.isDisabled ? 0.5 : 1;
+     const transition = 'opacity 300ms';
+ 
+     return { ...provided, opacity, transition };
+   }
+ }
+ 
+
+
 
 
 function Registration(props) {
    const [registrationForm, setregistrationForm] = React.useState({ firstname: '', lastname: '', email: '', password: ''});
+   const [streets, setStreets] = React.useState([]);
    const [error, setError] = React.useState('');
+   const [state,setState] = React.useState({selectedOption:null})
+   
    const history = useHistory();
+   var { selectedOption } = state;
+
+   React.useEffect(()=>{
+      fetch('/streets').then((data) => data.json()).then((streets) => setStreets(streets))
+      
+   },[])
+   const streets_array = []
+   streets.map((street)=> streets_array.push({label:street.name,value:street.name}))
+   //console.log(streets_array)
+
+
+
+
+
 
    function onChange(event) {
       const { name, value } = event.target;
       setregistrationForm(oldForm => ({...oldForm, [name]: value}))
    }
 
+   
+    function handleChange(selectedOption) {
+      setState({ selectedOption });
+      console.log(selectedOption)
+     
+    }
+    
    function onSubmit(e) {
       e.preventDefault();
       setError("");
@@ -22,7 +75,8 @@ function Registration(props) {
          firstname: registrationForm.firstname,
          lastname: registrationForm.lastname,
          email: registrationForm.username,
-         password: registrationForm.password
+         password: registrationForm.password,
+         selectedOption:state.selectedOption
       }
       const options = {
          method: 'POST',
@@ -44,7 +98,6 @@ function Registration(props) {
    }
 
 
-
    return (
       <Card>
          <div className='Login'>
@@ -64,6 +117,12 @@ function Registration(props) {
                <div className='FormRow'>
                   <label>Password</label>
                   <input name='password' required type='password' onChange={onChange} value={ registrationForm.password}/>
+               </div>
+			    <div className='FormRow'>
+                  <label>Address</label>
+                  <Select value={selectedOption}  onChange = {handleChange} styles={customStyles} placeholder="Select your address"
+                   options={streets_array}
+               />
                </div>
                <div className='error'>{error}</div>
                <button className='submit' type='submit'>Register</button>
