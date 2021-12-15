fbf8ddb81e325c8b4bd94770f10414eb50db8a56
Danijel Barišić <danijel.bar17@gmail.com>
2021-12-15 14:08:46 +0100

    Implementiran home service

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/home/HomeServiceJPA.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/home/HomeServiceJPA.java
@@ -1,8 +1,62 @@
 package progi.project.mojkvart.home;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import progi.project.mojkvart.meeting.Meeting;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class HomeServiceJPA implements HomeService{
     @Autowired
-    private HomeRepository homeRepo;
+    private HomeRepository homeRepository;
+
+    @Override
+    public Optional<Home> findByNumberAndStreetName(long homeNum, String name){
+        return homeRepository.findByNumberAndStreetName(homeNum,name);
+    }
+
+    @Override
+    public List<Home> listAll() {
+        return homeRepository.findAll();
+    }
+
+    @Override
+    public Home fetch(long homeId) {
+        return findById(homeId).orElseThrow(
+                IllegalArgumentException::new
+        );
+    }
+
+    @Override
+    public Optional<Home> findById(long homeId) {
+        Assert.notNull(homeId, "Id must be given");
+        return homeRepository.findById(homeId);
+    }
+
+    @Override
+    public Home createHome(Home home) {
+        Assert.isNull(home.getId(), "Home Id must be null, not: " + home.getId());
+        return homeRepository.save(home);
+    }
+
+    @Override
+    public Home updateHome(Home home) {
+        return homeRepository.save(home);
+    }
+
+    @Override
+    public Home deleteHome(long homeId) {
+        Home home = fetch(homeId);
+        homeRepository.delete(home);
+        return home;
+    }
+
+    @Override
+    public boolean existsById(long id) {
+        return findById(id).isPresent();
+    }
+
 }
