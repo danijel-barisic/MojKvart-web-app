e1e25aa9ff43ec382291cb5913268e9799e1cb63
Danijel Barišić <danijel.bar17@gmail.com>
2021-12-15 17:03:36 +0100

    Popravljena registracija (popravljen mehanizam adrese)

--- IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/registration/RegistrationRequest.java
+++ IzvorniKod/backend-spring-boot/src/main/java/progi/project/mojkvart/registration/RegistrationRequest.java
@@ -1,10 +1,14 @@
 package progi.project.mojkvart.registration;
 
+import progi.project.mojkvart.street.Street;
+
 public class RegistrationRequest {
     private final String firstname;
     private final String lastname;
     private final String email;
     private final String password;
+    private final long streetnumber;
+    private final Street street;
 
     public String getFirstname() {
         return firstname;
@@ -22,10 +26,21 @@ public class RegistrationRequest {
         return password;
     }
 
-    public RegistrationRequest(String firstname, String lastname, String email, String password) {
+    public Street getStreet() {
+        return street;
+    }
+
+    public long getStreetnumber() {
+        return streetnumber;
+    }
+
+    public RegistrationRequest(String firstname, String lastname, String email, String password,
+                               long streetnumber, Street street) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
         this.password = password;
+        this.streetnumber = streetnumber;
+        this.street = street;
     }
 }
